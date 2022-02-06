d7feca8b6a209097585f5bcd23ea47c5e45f4789
willardnyamombe <nya19001@byui.edu>
2022-02-05 21:21:01 -0700

    using nodemailer

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,13 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
+const transporter = nodemailer.createTransport(sendgridTransport({
+  auth: {
+    api_key: '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'
+  }
+}))
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
@@ -83,6 +90,15 @@ exports.postSignup = (req, res, next) => {
         })
         .then(result => {
           res.redirect('/login');
+          return transporter.sendMail({
+            to: email,
+            from: 'shop@node-complete.com',
+            subject: 'Signed succeeded!',
+            html: '<h1>You successfully signed up!<h1>'
+          });
+        })
+        .catch(err => {
+          console.log(err);
         });
     })
     .catch(err => {
